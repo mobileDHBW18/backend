9f1ddc3d21104ac251ff647f99bfb0ad6ced1543
Tobias Rahloff <rahloff@gmail.com>
2018-06-13 05:03:32 +0200

    working processing

--- meals/photo.js
+++ meals/photo.js
@@ -1,21 +1,27 @@
 'use strict'
 
+const AWS = require('aws-sdk')
 const Clarifai = require('clarifai')
 const nsfwClarifaiModelID = 'e9576d86d2004ed1a38ba0cf39ecb4b1'
 
+const s3 = new AWS.S3({ apiVersion: '2006-03-01' })
 const app = new Clarifai.App({
   apiKey: 'cd39d32638754a99b969a880c2f6317e'
 })
 
 module.exports.process = event => {
+  const region = process.env.REGION
+  const bucket = process.env.S3_BUCKET_PHOTOS
+
   event.Records.forEach(record => {
+    const key = record.s3.object.key
     const fileURL = [
       'https://s3.',
-      process.env.REGION,
+      region,
       '.amazonaws.com/',
-      process.env.S3_BUCKET_PHOTOS,
+      bucket,
       '/',
-      record.s3.object.key
+      key
     ].join('')
 
     app.models.predict(nsfwClarifaiModelID, fileURL)
@@ -24,10 +30,20 @@ module.exports.process = event => {
           const concepts = response.outputs[0].data.concepts
           const nsfw = concepts.find(c => c.name === 'nsfw').value > 0.15
 
+          if (nsfw) {
+            s3.deleteObject({Bucket: bucket, Key: key},
+              (err, data) => {
+                err
+                  ? console.log(err, err.stack)
+                  : console.log(`deleted ${key} in bucket ${bucket}`)
+              })
+          } else {
+            console.log('SFW')
+            /** HERE BE DYNAMO STUFF */
+          }
+
           console.log(`
             ${fileURL}
-
-            ${JSON.stringify(concepts, null, 2)}
 
             NSFW: ${nsfw}
           `)
